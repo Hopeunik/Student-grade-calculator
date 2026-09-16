f948373cb1a0947fb2539066e91675184c00cd9f
GRACE KAISAI SAIGILU <kaisaigrace50@gmail.com>
2026-09-17 00:07:13 +0300

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    if (operator == '+') {
+        result = num1 + num2;
+        printf("Result = %.2f\n", result);
+    }
+    else if (operator == '-') {
+        result = num1 - num2;
+        printf("Result = %.2f\n", result);
+    }
+    else if (operator == '*') {
+        result = num1 * num2;
+        printf("Result = %.2f\n", result);
+    }
+    else if (operator == '/') {
+        if (num2 != 0)
+            printf("Result = %.2f\n", num1 / num2);
+        else
+            printf("Cannot divide by zero.\n");
+    }
+    else {
+        printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
